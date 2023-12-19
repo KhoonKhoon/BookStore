ab4872482a2be93a281070a746cb3441b20cc30f
KhoonKhoon <khonsint1361@gmail,com>
2023-12-19 22:07:15 +0630

    added softdelete

--- app/Models/Author/Author.php
+++ app/Models/Author/Author.php
@@ -4,10 +4,11 @@ namespace App\Models\Author;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['name', 'dateofbirth','bornIn'];
 
     public function books()
--- app/Models/Book/Book.php
+++ app/Models/Book/Book.php
@@ -6,10 +6,11 @@ use App\Models\Author\Author;
 use App\Models\Category\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['name', 'author_id', 'category_id'];
 
     public function category()
